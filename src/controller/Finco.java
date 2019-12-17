dc6c92594750c8be099d9b2d9b7262040d2b197a
gfasil <fhabtegiorgis@mum.edu>
2019-12-17 09:18:26 -0600

    -added methods getOwner, hasaccount,addinterest, getcustomerlist to the finco -added one more line to add customers to the list after creating an account

--- src/controller/Finco.java
+++ src/controller/Finco.java
@@ -1,46 +1,80 @@
 package controller;
 
 import model.Account;
+import model.IAccount;
 import model.ICustomer;
 import model.IOperation;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiPredicate;
 
 public class Finco {
 
-	private static List<ICustomer> customers = new ArrayList<ICustomer>();
+    private static List<ICustomer> customers = new ArrayList<ICustomer>();
 
-	private static IFincoAbstractFactory fincoFactory = new FincoFactory();
+    private static IFincoAbstractFactory fincoFactory = new FincoFactory();
 
-	private static ICustomer findCustomer(ICustomer customer) {
-		return customers.stream().filter(c -> c.equals(customer)).findFirst().orElse(customer);
-	}
+    private static ICustomer findCustomer(ICustomer customer) {
+        return customers.stream().filter(c -> c.equals(customer)).findFirst().orElse(customer);
+    }
 
-	public static void addCompanyAccount(String name, String email, String street, String city, String state,
-			String zipcode, int numOfEmployees) {
-		ICustomer customer = fincoFactory.createCompany(name, email, street, city, state, zipcode, numOfEmployees);
-		customer = findCustomer(customer);
-		customer.addAccount(fincoFactory.createAccount(customer));
-	}
+    public static void addCompanyAccount(String name, String email, String street, String city, String state,
+                                         String zipcode, int numOfEmployees) {
+        ICustomer customer = fincoFactory.createCompany(name, email, street, city, state, zipcode, numOfEmployees);
+        customer = findCustomer(customer);
+        customer.addAccount(fincoFactory.createAccount(customer));
+        customers.add(customer);
+    }
 
-	public static void addPersonAccount(String name, String email, String street, String city, String state,
-			String zipcode, LocalDate birthDate) {
-		ICustomer customer = fincoFactory.createPerson(name, email, street, city, state, zipcode, birthDate);
-		customer = findCustomer(customer);
-		customer.addAccount(fincoFactory.createAccount(customer));
+    public static void addPersonAccount(String name, String email, String street, String city, String state,
+                                        String zipcode, LocalDate birthDate) {
+        ICustomer customer = fincoFactory.createPerson(name, email, street, city, state, zipcode, birthDate);
+        customer = findCustomer(customer);
+        customer.addAccount(fincoFactory.createAccount(customer));
+        customers.add(customer);
 
-	}
+    }
 
-	public static void addAccountOperation(Account acc, String name, double amount) {
-		IOperation newOpp = fincoFactory.createOperation(name, amount);
-		acc.applyOperation(newOpp);
-	}
+    public static void addAccountOperation(Account acc, String name, double amount) {
+        IOperation newOpp = fincoFactory.createOperation(name, amount);
+        acc.applyOperation(newOpp);
+    }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    protected static BiPredicate<ICustomer, String> hasAccount = (customer, accountNumber) -> customer.getAccountList().stream()
+            .anyMatch(account -> account.getAccountNumber().equals(accountNumber));
 
-	}
+    public static ICustomer getOwner(String accountNumber) {
+        return customers.stream()
+                .filter(customer -> hasAccount.test(customer, accountNumber)).findFirst().get();
+    }
+
+    public static IAccount getAccount(String accountNumber, ICustomer customer) {
+        return customer.getAccountList().stream().filter(x -> x.getAccountNumber().equals(accountNumber)).findFirst().orElse(null);
+//		for (IAccount temp : customer.getAccountList()) {
+//			if (temp.getAccountNumber().equals(accountNumber))
+//				return temp;
+//		}
+//		return null;
+    }
+
+    public static List<ICustomer> getCustomers() {
+        return customers;
+    }
+
+    public static void addInterest() {
+        customers.stream().map((ICustomer x) -> x.getAccountList()).forEach(a -> a.stream().forEach(b -> b.addInterest()));
+//		for (ICustomer c : customers) {
+//			for (IAccount a : c.getAccountList()) {
+//				a.addInterest();
+//			}
+//		}
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
---
+++ src/util/SetGulFunctor.java
@@ -0,0 +1,10 @@
+package util;
+
+import javax.swing.table.DefaultTableModel;
+
+public interface SetGulFunctor {
+
+    public  void setCols(DefaultTableModel model);
+    public  void setButtons(String button1, String button2);
+
+}
