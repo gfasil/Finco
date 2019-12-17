f67c7d1c8d50f66a86c0297512db90b34b509348
Yusuf Hussein <yusuf.hussein8894@gmail.com>
2019-12-16 18:34:05 -0600

    Override equals and hashcode in Customer

--- src/model/AbstractCustomer.java
+++ src/model/AbstractCustomer.java
@@ -67,13 +67,40 @@ public abstract class AbstractCustomer implements ICustomer {
 		System.out.println( msg + " "+ "sent to: " + this.name + " - " + this.email);
 	}
 	
-	//Is Customer name and email is equal to existing customer?
 	@Override
-	public boolean equals(Object customer) {
-        if (customer == null) return false;
-        if (!(customer instanceof AbstractCustomer)) return false;
-		AbstractCustomer c = (AbstractCustomer)customer;
-        if(this.name.equals(c.name) && this.email.equals(c.email)) return true;
-        return false;
-    }
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AbstractCustomer other = (AbstractCustomer) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 }
